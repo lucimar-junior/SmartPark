1bcca3767778e9b1538b7d41481038a83140ee75
Lucimar Junior <lucimar_junior@outlook.com.br>
2018-04-07 17:13:51 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/smartpark/smartpark/MeusVeiculos.java
@@ -0,0 +1,13 @@
+package br.com.smartpark.smartpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeusVeiculos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_veiculos);
+    }
+}
---
+++ app/src/main/java/br/com/smartpark/smartpark/PrincipalActivity.java
@@ -0,0 +1,61 @@
+package br.com.smartpark.smartpark;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class PrincipalActivity extends AppCompatActivity {
+    ImageButton btnMeusVeiculos;
+    ImageButton btnNovoVeiculo;
+    ImageButton btnConsultarAgendamento;
+    ImageButton btnAlterarCadastro;
+    ImageButton btnNovoAgendamento;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        btnMeusVeiculos = findViewById(R.id.btnMeusVeiculos);
+        btnMeusVeiculos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, MeusVeiculos.class));
+            }
+        });
+
+        btnNovoVeiculo = findViewById(R.id.btnNovoVeiculo);
+        btnNovoVeiculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, CadastroVeiculos.class));
+            }
+        });
+
+        /*btnConsultarAgendamento = findViewById(R.id.btnConsultarAgendamento);
+        btnConsultarAgendamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, PrincipalActivity.class));
+            }
+        });
+
+        btnAlterarCadastro = findViewById(R.id.btnAlterarCadastro);
+        btnAlterarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, PrincipalActivity.class));
+            }
+        });
+
+        btnNovoAgendamento = findViewById(R.id.btnNovoAgendamento);
+        btnNovoAgendamento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PrincipalActivity.this, PrincipalActivity.class));
+            }
+        });*/
+    }
+}
